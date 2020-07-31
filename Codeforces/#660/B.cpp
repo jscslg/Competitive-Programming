b16f16ed1585f43513244e5f424f126f119f9c8d
jscslg <jscslg27@gmail.com>
2020-08-01 01:39:41 +0530

    update

--- Codeforces/#660/B.cpp
+++ Codeforces/#660/B.cpp
@@ -6,3 +6,17 @@
 #define fast ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
 
 using namespace std; 
+
+int main(){
+    fast;
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;cin>>n;
+        ll m=(n+3)/4;
+        f(i,0,n-m) cout<<9;
+        f(i,0,m) cout<<8;
+        cout<<endl;
+    }
+    return 0;
+}
